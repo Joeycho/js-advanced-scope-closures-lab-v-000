5ec8f54d61cb3f4c3679357c6780a75668f8badf
Joey Cho <cjy12182000@naver.com>
2018-02-08 15:10:40 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,35 @@
+function produceDrivingRange(range){
+  return function checkDistance(distOne, distTwo){
+    let rangeInt = parseInt(range)
+    let distOneInt = parseInt(distOne)
+    let distTwoInt = parseInt(distTwo)
+
+    let diff = Math.abs(distOneInt-distTwoInt)
+
+//    return Math.abs(diff-rangeInt)
+
+    if(diff<=rangeInt){
+      return `within range by ${rangeInt-diff}`
+    }else{
+      return `${diff-rangeInt} blocks out of range`
+    }
+
+  }
+}
+
+function produceTipCalculator(percent){
+  return function calculate(fare){
+    return fare*percent
+  }
+}
+
+function createDriver(){
+  let driverId = 0
+
+  return class Driver{
+    constructor(name){
+      this.name = name
+      this.id = driverId++
+    }
+  }
+  }
