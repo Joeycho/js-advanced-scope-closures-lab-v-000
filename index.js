5afb2cc763d598779d1243959fbd16dac1976a76
Joey Cho <cjy12182000@naver.com>
2018-02-08 15:06:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function produceDrivingRange(range){
 
     let diff = Math.abs(distOneInt-distTwoInt)
 
-    return diff-rangeInt
+    return Math.abs(diff-rangeInt)
 /*
     if(diff=<rangeInt){
       return `within range by ${rangeInt-diff}`
