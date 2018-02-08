24cd1914f3b6b2e512c4b1fc01ce42acd6497cf0
Joey Cho <cjy12182000@naver.com>
2018-02-08 14:56:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 function produceDrivingRange(range){
-  return function checkDistance(distance){
+  return function checkDistance(dist1, dist2){
     let rangeInt = parseInt(range)
-    let distanceInt = parseInt(distance)
+    let dist1Int = parseInt(dist1)
+    let dist2Int = parseInt(dist2)
 
+    let diff = Math.abs(dist1Int-dist2Int)
 
-    if(rangeInt>=distanceInt){
-      return `within range by ${rangeInt-distanceInt}`
+    if(diff=<rangeInt){
+      return `within range by ${rangeInt-diff}`
     }else{
-      return `${distanceInt-rangeInt} blocks out of range`
+      return `${diff-rangeInt} blocks out of range`
     }
 
 
